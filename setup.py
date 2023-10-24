f03243da38016ca29c735c56ad1deb341ab92494
Robert Lacina <robert@lacina.dev>
2023-10-24 16:03:13 +0200

    lcd nav and more

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['navi_manager'],
+    packages=['navi_man'],
     package_dir={'': 'src'}
 )
 
